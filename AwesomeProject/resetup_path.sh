0e964eb44a40cd9286097d1d43c2429626fa6a62
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2018-01-12 21:13:44 -0800

    make it work with hot reload; add image; change background

---
+++ AwesomeProject/resetup_path.sh
@@ -0,0 +1,5 @@
+# setting up some android sdk stuff
+# export ANDROID_HOME=$HOME/Desktop/ANDROID_SDK
+export ANDROID_HOME=/Users/danbern/Desktop/ANDROID_SDK
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
