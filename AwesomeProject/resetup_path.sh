8d01eaed6f11c8176d7b32116bfcd6272bd60e98
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2018-01-16 21:31:16 -0800

    some files got left out :(

--- AwesomeProject/resetup_path.sh
+++
@@ -1,5 +0,0 @@
-# setting up some android sdk stuff
-# export ANDROID_HOME=$HOME/Desktop/ANDROID_SDK
-export ANDROID_HOME=/Users/danbern/Desktop/ANDROID_SDK
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/platform-tools
