3e7b72a8f6b3f3b77a4cb264dffa2b08524534f4
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2018-01-23 12:31:28 -0800

    change app icon

--- FirstApp01/App.js
+++ FirstApp01/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button, TextInput, TouchableOpacity } from 'react-native';
 
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
