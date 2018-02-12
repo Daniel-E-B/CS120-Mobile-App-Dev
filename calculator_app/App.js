597a14609a643a244eafaf1250c0ecb33b479a2f
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2018-02-12 10:54:05 -0800

    complete base tutorial

--- calculator_app/App.js
+++ calculator_app/App.js
@@ -1,58 +1,121 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
   Text,
-  View
+  View,
+  StyleSheet
 } from 'react-native';
+import Style from './components/Style';
+import InputButton from './components/InputButton';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// Define the input buttons that will be displayed in the calculator.
+const inputButtons = [
+  [1, 2, 3, '/'],
+  [4, 5, 6, '*'],
+  [7, 8, 9, '-'],
+  [0, '.', '=', '+'],
+  ['CLEAR']
+];
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class ReactCalculator extends Component {
+  constructor(props) {
+    super(props);
+    this._onInputButtonPressed = this._onInputButtonPressed.bind(this);
+
+    this.state = {
+      previousInputValue: 0,
+      inputValue: 0,
+      selectedSymbol: null
+    }
+  }
+
+  _renderInputButtons() {
+    let views = [];
+    for (let i = 0; i < inputButtons.length; i++) {
+      views.push([]);
+      let viewRow = [];
+      for (let j = 0; j < inputButtons[i].length; j++) {
+        let input = inputButtons[i][j];
+        viewRow.push(<InputButton
+          value={input}
+          highlight={this.state.selectedSymbol === input}
+          onPress={() => this._onInputButtonPressed(input)}
+          key={i + " " + j}
+        />);
+      }
+      views[i].push(<View style={Style.inputRow}
+        key={i}>
+        {viewRow}
+      </View>
+      );
+    }
+    return views;
+  }
+
+  _onInputButtonPressed(input) {
+    switch (typeof input) {
+      case 'number':
+        return this._handleNumberInput(input)
+      case 'string':
+        return this._handleStringInput(input)
+    }
+  }
+
+  _handleStringInput(str) {
+    switch (str) {
+      case '/':
+      case '*':
+      case '+':
+      case '-':
+        this.setState({
+          selectedSymbol: str,
+          previousInputValue: this.state.inputValue,
+          inputValue: 0
+        });
+        break;
+      case '=':
+        let symbol = this.state.selectedSymbol,
+          inputValue = this.state.inputValue,
+          previousInputValue = this.state.previousInputValue;
+
+        if (!symbol) {
+          return;
+        }
+
+        this.setState({
+          previousInputValue: 0,
+          inputValue: eval(previousInputValue + symbol + inputValue),
+          selectedSymbol: null
+        });
+        break;
+    }
+  }
+
+  _handleNumberInput(num) {
+    let inputValue = (this.state.inputValue * 10) + num;
+
+    this.setState({
+      inputValue: inputValue
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={Style.rootContainer}>
+        <View style={Style.displayContainer}>
+          <Text style={Style.displayText}>{this.state.inputValue}</Text>
+        </View>
+        <View style={Style.inputContainer}>
+          {this._renderInputButtons()}
+        </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    // alignItems: 'center',
+    // justifyContent: 'center'
+  }
+})
