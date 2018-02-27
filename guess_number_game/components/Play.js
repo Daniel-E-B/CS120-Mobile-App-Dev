14f2e6f3e1a63193a84de765c6d4bfab5e0e779a
Daniel-E-B <GOAF-Pilot@users.noreply.github.com>
2018-02-27 11:19:13 -0800

    add empty classes to components

---
+++ guess_number_game/components/Play.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class Play extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+    render() {
+        return ({
+
+        })
+    }
+}
