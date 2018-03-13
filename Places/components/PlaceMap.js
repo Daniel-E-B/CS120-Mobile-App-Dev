b2c5bdd49db8585bedfae686ffbec929c43540ea
Daniel-E-B <GOAF-Pilot@users.noreply.github.com>
2018-03-13 11:17:01 -0700

    remove duplicate import

--- Places/components/PlaceMap.js
+++ Places/components/PlaceMap.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import MapView from 'react-native-maps';
 import MapView, { Marker } from 'react-native-maps';
 
 export default class PlaceMap extends Component {
