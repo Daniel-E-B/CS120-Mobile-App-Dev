ac8a027dfb93db52885a1853e8a53ef87bce8862
Daniel-E-B <GOAF-Pilot@users.noreply.github.com>
2018-03-19 10:39:20 -0700

    get up to add a marker to our map section in tutorial 2

--- Places/components/AddPlace.js
+++ Places/components/AddPlace.js
@@ -1,13 +1,92 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 
 export default class AddPlace extends Component {
 
+  constructor() {
+    super();
+    this.state = { latitude: 0, longitude: 0, title: "", description: "", }
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Add a Place Here!</Text>
+      <View style={styles.addPlaceScreen}>
+        <View style={styles.description}>
+          <Text> Add a Place! Enter a latitude, longitude, title and description. </Text>
+        </View>
+        <View style={styles.input}>
+          <TextInput style={styles.textInputShort}
+            placeholder="Latitude"
+            keyboardType="numeric"
+            onChangeText={(text) => { this.setState({ latitude: parseFloat(text) }) }}
+          />
+          <TextInput style={styles.textInputShort}
+            placeholder="Longitude"
+            keyboardType="numeric"
+            onChangeText={(text) => { this.setState({ longitude: parseFloat(text) }) }}
+          />
+          <TextInput style={styles.textInputShort}
+            placeholder="Title"
+            onChangeText={(text) => { this.setState({ title: text }) }}
+          />
+          <TextInput style={styles.textInputLong}
+            placeholder="Description"
+            onChangeText={(text) => { this.setState({ description: text }) }}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            onPress={() => { }}
+            title="Add Marker"
+
+          />
+        </View>
       </View>
-    );
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  description: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+  },
+  button: {
+    padding: 12,
+    borderRadius: 6,
+    flex: 1
+  },
+  textInputShort: {
+    height: 40,
+    width: 200,
+    padding: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 5
+  },
+  textInputLong: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    height: 80,
+    width: 200,
+    padding: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 5
+  },
+  addPlaceScreen: {
+    flex: 3,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  input: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+})
