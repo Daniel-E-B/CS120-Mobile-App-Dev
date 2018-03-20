bdec8e675f95c68b5f372a9c846771d0343579f8
Daniel-E-B <GOAF-Pilot@users.noreply.github.com>
2018-03-20 11:47:50 -0700

    get rid of broken code that wasnt doing anything

--- Places/App.js
+++ Places/App.js
@@ -1,42 +1,20 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import PlaceMap from './components/PlaceMap';
 import AddPlace from './components/AddPlace';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabNav = TabNavigator(
   {
-    FavoritePlaces: { screen: PlaceMap },
-    AddPlace: { screen: AddPlace },
+    FavoritePlaces: { screen: PlaceMap},
+    AddPlace: { screen: AddPlace}
   },
   {
     tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    },
-    // ********************** //
-    // below is the code you need to add //
-    // ********************** //
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === 'FavoritePlaces') {
-          let suffix = focused == true ? '' : '-outline';
-          iconName = 'ios-map' + suffix;
-
-        } else if (routeName === 'AddPlace') {
-          let suffix = focused == true ? '' : '-outline';
-          iconName = 'ios-add-circle' + suffix;
-        }
-
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
-        // You can see the full list of icons here: https://ionicframework.com/docs/ionicons/
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      }
-    }),
+      activeTintColor: 'white',
+      inactiveTintColor: '#dddddd',
+    }
+    // couldn't get icons to work, so didn't use them
   }
 )
 
@@ -49,8 +27,12 @@ export default class App extends Component {
     this.updateMarkers = this.updateMarkers.bind(this);
   }
 
+  componentDidMount() {
+    StatusBar.setHidden(true);
+  }
+
   updateMarkers(marker) {
-    let stateMarkers = this.state.markers.slice();
+    let stateMarkers = this.state.markers.slice(); // copy it, don't get a reference
     stateMarkers.push(marker);
     this.setState({
       markers: stateMarkers
@@ -59,8 +41,8 @@ export default class App extends Component {
   render() {
     return (
       <TabNav
-        screenProp={{ 
-          markers: this.state.markers, 
+        screenProp={{
+          markers: this.state.markers,
           update: this.updateMarkers,
         }}
       />
--- Places/components/PlaceMap.js
+++ Places/components/PlaceMap.js
@@ -8,13 +8,14 @@ export default class PlaceMap extends Component {
   render() {
     return (
       <MapView style={styles.map}
+        mapType="satellite"
         initialRegion={{
           latitude: 38.897902,
           longitude: -77.036508,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
           mapType: 'standard',
-          title: "Nueva School"
+          title: "Casablanca"
         }}>
       </MapView>
     )
